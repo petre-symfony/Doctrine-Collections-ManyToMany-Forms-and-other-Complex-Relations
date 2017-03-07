61e6d6b4586fdeaddb210991e356566dbfda3e7a
petrero <petrero@gmail.com>
2017-03-07 22:23:35 +0200

    Deleting the Link
    Now all we need to do is remove the User from the Genus. We don't have a method to do that yet, so right below addGenusScientist(), make a new public function called removeGenusScientist() with a User argument.
    Inside, it's so simple: {dollarSign}this->genusScientists->removeElement({dollarSign}user)
    In other words, just remove the User from the array... by using a fancy convenience method on the collection. That doesn't touch the database yet: it just modifies the array.

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -204,6 +204,10 @@ class Genus
       $this->genusScientists[] = $user;
     }
     
+    public function removeGenusScientist(User $user) {
+      $this->genusScientists->removeElement($user);
+    }
+    
     /**
      * 
      * @return ArrayCollection|User[]
