162b056464714dec774807266d29080da39cebad
petrero <petrero@gmail.com>
2017-03-08 19:32:56 +0200

    Copy code from previous branch

--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -6,5 +6,9 @@ use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
-
+  public function createIsScientistQueryBuilder(){
+    return $this->createQueryBuilder('user')
+      ->andWhere('user.isScientist = :isScientist')
+      ->setParameter('isScientist', true);
+  }
 }
