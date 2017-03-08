1f7a7e92573e95785c39c441a043fc4b0a7d1e8b
petrero <petrero@gmail.com>
2017-03-08 22:55:00 +0200

    Take the last line: with ADD PRIMARY KEY id, copy it, remove that line, and then - after the id is added in the previous query - paste it and add a comma.
    MySQL needs this to happen all in one statement.
    
    But now, our migrations are in a crazy weird state, because this one partially ran. So let's start from scratch: drop the database fully, create the database, and then make sure all of our migrations can run from scratch:
    
    bin/console doctrine:database:drop --force
    bin/console doctrine:database:create
    bin/console doctrine:migrations:migrate
    
    Success!
    
    Now that we have a different type of relationship, our app is broken! Yay! Let's fix it and update our forms to use the CollectionType.

--- app/DoctrineMigrations/Version20170308224004.php
+++ app/DoctrineMigrations/Version20170308224004.php
@@ -21,10 +21,9 @@ class Version20170308224004 extends AbstractMigration
         $this->addSql('ALTER TABLE genus_scientist DROP FOREIGN KEY FK_66CF3FA885C4074C');
         $this->addSql('ALTER TABLE genus_scientist DROP FOREIGN KEY FK_66CF3FA8A76ED395');
         $this->addSql('ALTER TABLE genus_scientist DROP PRIMARY KEY');
-        $this->addSql('ALTER TABLE genus_scientist ADD id INT AUTO_INCREMENT NOT NULL, ADD years_studied VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE genus_scientist ADD id INT AUTO_INCREMENT NOT NULL, ADD PRIMARY KEY (id), ADD years_studied VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE genus_scientist ADD CONSTRAINT FK_66CF3FA885C4074C FOREIGN KEY (genus_id) REFERENCES genus (id)');
         $this->addSql('ALTER TABLE genus_scientist ADD CONSTRAINT FK_66CF3FA8A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE genus_scientist ADD PRIMARY KEY (id)');
     }
 
     /**
